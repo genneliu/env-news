51d04e58dfe8731506742543701a827319266d07
Genne Liu <genneliu@gmail.com>
2021-10-04 10:39:06 -0700

    first feature made, need style

---
+++ server/controller.js
@@ -0,0 +1,40 @@
+const { default: axios } = require("axios");
+
+//api key hidden for small safety measure
+const newsAPIKey = process.env.NEWS_API_KEY;
+
+//const for safety
+const query = "climate";
+
+module.exports = {
+    getArticles: (req, response) => {
+        axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=${newsAPIKey}`)
+        .then(function (res) {
+            let articles = res.data.articles
+            //want to see what response looks like
+            console.log(articles)
+            //send response back
+            response.status(200).send(articles);
+            //have frontend create loop and sort data to view
+
+            //need error code 
+            if (res == null || res == 'undefined') {
+                res.sendStatus(404);
+            };
+        });
+    },
+}
+
+
+// https://newsapi.org/docs/endpoints/everything
+
+// module.exports = {
+//     getHouses: (req, res) => {
+//         res.status(200).send(houses);
+//     },
+//     deleteHouse: (req, res) => {
+//         const {id} = req.params
+//         let index = houses.findIndex(elem => elem.id === +id)
+//         houses.splice(index, 1)
+//         res.status(200).send(houses)
+//     },
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const axios = require("axios")
+const path = require("path")
+
+//create server object
+const app = express()
+app.use(express.static("client"));
+
+const controller = require('./controller.js');
+
+//responding to paths with respective file
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, "../client/index.html"))
+})
+
+app.get('/script.js', (req, res) => {
+res.sendFile(path.join(__dirname, '../client/script.js'))
+})
+
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
+    })
+
+// app.get('/script.js', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/script.js'))
+//     })
+
+app.get('/api/articles', controller.getArticles); 
+// app.post('/api/houses', controller.createHouse);
+// app.put('/api/houses/:id', controller.updateHouse);
+// app.delete('/api/houses/:id', controller.deleteHouse);
+
+// app.get("/api/sad", (req, res) => {
+//     const sad= ["Sorry you feel that way :("];
+//     res.status(200).send(sad);
+//   });
+  
+//   app.get("/api/angry", (req, res) => {
+//     const angry= ["Take some deep breaths"];
+//     res.status(200).send(angry);
+//   });
+
+const port = process.env.PORT || 4400
+
+app.listen(port, () => {
+    console.log(`ahoy matey: ${port}`)
+})
